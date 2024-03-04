83cc207ea9bdc3312d7dd2ef071a0ff2d7cd0a30
luiscarlos <luiscarlos8pe@gmail.com>
2024-03-04 18:44:59 -0300

    Criando projeto e entidade cliente

---
+++ src/main/java/br/com/lc/clientes/ClientesBeckApplication.java
@@ -0,0 +1,13 @@
+package br.com.lc.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesBeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesBeckApplication.class, args);
+	}
+
+}
